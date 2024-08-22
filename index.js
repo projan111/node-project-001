c8380167b2718dd937faa1f49417b2c2cabb9fd8
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-08-22 16:16:50 +0545

    group same routes

--- index.js
+++ index.js
@@ -21,4 +21,25 @@ app.get("/api/users", (req, res) => {
   res.json(users);
 });
 
+app.get("/api/users/:uid", (req, res) => {
+  const id = Number(req.params.uid);
+  //Search Query for user id in this case
+  const user = users.find((user) => user.id === id);
+
+  return res.json(user);
+});
+
+//Grouping the same routes
+app
+  .route("/api/users/:uid")
+  .get((req, res) => {
+    const id = Number(req.params.uid);
+    //Search Query for user id in this case
+    const user = users.find((user) => user.id === id);
+
+    return res.json(user);
+  })
+  .post((req, res) => {})
+  .delete((req, res) => {});
+
 app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
