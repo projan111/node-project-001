00c05afdc17bdd98f8e08937e14d119c09df91cc
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-08-26 19:33:34 +0545

    add middleware

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -2,18 +2,23 @@ const shortid = require("shortid");
 const URL = require("../models/url");
 
 async function handleGenerateNewShortURL(req, res) {
-  const body = req.body;
+  try {
+    const body = req.body;
 
-  if (!body.url) return res.status(400).json({ error: "url is required" });
-  const shortId = shortid();
+    if (!body.url) return res.status(400).json({ error: "url is required" });
+    const shortId = shortid();
 
-  await URL.create({
-    shortId: shortId,
-    redirectURL: body.url,
-    visitHistory: [],
-  });
+    await URL.create({
+      shortId: shortId,
+      redirectURL: body.url,
+      visitHistory: [],
+      createdBy: req.user._id,
+    });
 
-  return res.render("home", { id: shortId });
+    return res.render("home", { id: shortId });
+  } catch (error) {
+    console.log("something is going wrong here", error);
+  }
 }
 
 async function handleGetAnalytics(req, res) {
