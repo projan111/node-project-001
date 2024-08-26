3eee3f4b2c9fffeb70bee43173e8388dab5cca95
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-08-26 18:34:00 +0545

    add cookies

--- controllers/loginUserController.js
+++ controllers/loginUserController.js
@@ -1,4 +1,6 @@
+const { v4: uuidv4 } = require("uuid");
 const { LoginUser } = require("../models/userModel");
+const { setUser } = require("../services/auth");
 
 async function handleLoginUserSignup(req, res) {
   const { name, email, password } = req.body;
@@ -23,6 +25,9 @@ async function handleLoginUserLogin(req, res) {
 
   if (!user) return res.json({ error: "Incorrect Email or Password" });
 
+  const sessionId = uuidv4();
+  setUser(sessionId, user);
+  res.cookie("uid", sessionId);
   return res.redirect("/");
 }
 
