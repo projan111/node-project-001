935c1ef9a94cf68d293e420d9188f2c95f6de152
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-08-28 15:47:55 +0545

    add auth and authorixation standard methods

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,39 +1,67 @@
 const { getUser } = require("../services/auth");
 
-async function restictToLogginedInOnly(req, res, next) {
-  try {
-    // const userUId = req.cookies?.uid;
-    const userUId = req.headers["authorization"];
-    if (!userUId) return res.redirect("/login");
+const checkForAuthentication = (req, res, next) => {
+  const authorizationHeaderValue = req.headers["authorization"];
 
-    const token = userUId.split("Bearer ")[1]
+  req.user = null;
 
-    const user = getUser(token);
+  if (
+    !authorizationHeaderValue ||
+    !authorizationHeaderValue.startsWith("Bearer")
+  )
+    return next();
 
-    if (!user) return res.redirect("/login");
+  const token = authorizationHeaderValue.split("Bearer ")[1];
+  const user = getUser(token);
 
-    req.user = user;
-    next();
-  } catch (error) {
-    console.error("Error in restrictToLoggedInOnly middleware:", error);
-    return res.redirect("/login");
-  }
-}
+  req.user = user;
+  return next();
+};
 
-async function checkAuth(req, res, next) {
-  try {
-    // const userUId = req.cookies?.uid;
-    const userUId = req.headers["authorization"];
-    const token = userUId.split("Bearer ")[1];
+const restrictTo = (roles = []) => {
+  return function (req, res, next) {
+    if (!req.user) return res.redirect("/login");
 
-    const user = getUser(token);
+    if (!roles.includes(req.user.role)) return res.end("Un Authorized");
 
-    req.user = user;
-    next();
-  } catch (error) {
-    console.error("Error in restrictToLoggedInOnly middleware:", error);
-    return res.redirect("/login");
-  }
-}
+    return next();
+  };
+};
 
-module.exports = { restictToLogginedInOnly, checkAuth };
+// async function restictToLogginedInOnly(req, res, next) {
+//   try {
+//     // const userUId = req.cookies?.uid;
+//     const userUId = req.headers["authorization"];
+//     if (!userUId) return res.redirect("/login");
+
+//     const token = userUId.split("Bearer ")[1];
+
+//     const user = getUser(token);
+
+//     if (!user) return res.redirect("/login");
+
+//     req.user = user;
+//     next();
+//   } catch (error) {
+//     console.error("Error in restrictToLoggedInOnly middleware:", error);
+//     return res.redirect("/login");
+//   }
+// }
+
+// async function checkAuth(req, res, next) {
+//   try {
+//     // const userUId = req.cookies?.uid;
+//     const userUId = req.headers["authorization"];
+//     const token = userUId.split("Bearer ")[1];
+
+//     const user = getUser(token);
+
+//     req.user = user;
+//     next();
+//   } catch (error) {
+//     console.error("Error in restrictToLoggedInOnly middleware:", error);
+//     return res.redirect("/login");
+//   }
+// }
+
+module.exports = { checkForAuthentication, restrictTo };
